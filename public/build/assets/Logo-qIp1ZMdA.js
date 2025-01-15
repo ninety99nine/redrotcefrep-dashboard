b4ab1d54ef1179fc9e988adbe608a140118d5728
Julian Tabona <brandontabona@gmail.com>
2025-01-15 22:20:31 +0200

    Update 15 JAN 2024 22:21

---
+++ public/build/assets/Logo-qIp1ZMdA.js
@@ -0,0 +1,1 @@
+import{o as t,c as r,n as o}from"./app-EZwFWzQg.js";import{_ as s}from"./_plugin-vue_export-helper-DlAUqK2U.js";const a={props:{height:{type:String,default:"h-20"}}};function n(c,l,e,p,i,m){return t(),r("img",{class:o(e.height),src:"/images/logo-black-transparent.png",alt:"Perfect Order"},null,2)}const g=s(a,[["render",n]]);export{g as L};
