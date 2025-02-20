7997aa8897ace8e3feaf1b31edcbc04ba183a761
Julian Tabona <brandontabona@gmail.com>
2025-02-20 15:37:47 +0200

    Update 20 FEB 2025 15:37

--- resources/js/pages/transactions/TransactionStatus.vue
+++ resources/js/pages/transactions/TransactionStatus.vue
@@ -7,28 +7,38 @@
             <div class="flex flex-col items-center mb-10">
 
                 <h1 class="flex flex-col items-center space-y-2 text-xl text-gray-700 font-semibold mb-4">
-                    <span class="text-4xl">🎉</span>
+                    <span v-if="!isLoadingTransaction && isPaid" class="text-4xl">🎉</span>
                     <span>Transaction Status</span>
                 </h1>
 
                 <p class="w-96 text-sm text-gray-500 text-center">
-                    {{ transactionDescription }}
+                    <template v-if="isLoadingTransaction">
+                        Hang tight! We're checking the transaction status
+                    </template>
+                    <template v-else-if="isPaid">
+                        This transaction has been paid successully
+                    </template>
+                    <template v-else-if="isPendingPayment">
+                        This transaction is still waiting to payment
+                    </template>
+                    <template v-else-if="isFailedPayment">
+                        This transaction has failed
+                    </template>
                 </p>
 
             </div>
 
-            <!-- Loader -->
-            <div
-                v-if="isLoadingTransaction"
-                class="flex justify-center">
-                <SpinningLoader></SpinningLoader>
+            <div class="flex flex-col items-center mb-8">
+
+                <!-- Rooftop Image -->
+                <img src="/images/store-rooftop.png" class="w-96">
+
+                <!-- Loader -->
+                <SpinningLoader v-if="isLoadingTransaction" class="mt-16"></SpinningLoader>
+
             </div>
 
             <template v-if="!isLoadingTransaction">
-
-                <div class="flex justify-center mb-8">
-                    <img src="/images/store-rooftop.png" class="w-96">
-                </div>
 
                 <div class="flex justify-center space-x-4 mb-8">
 
@@ -38,6 +48,10 @@
                         <div class="flex items-center space-x-2 mb-2">
                             <svg v-if="isPaid" class="w-8 h-8 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+
+                            <svg v-if="isPendingPayment" class="w-8 h-8 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
                             </svg>
 
                             <svg v-if="isFailedPayment" class="w-8 h-8 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
@@ -72,31 +86,54 @@
                             </div>
                         </div>
 
-                        <div class="flex items-center space-x-2 text-xs mb-2">
-                            <span class="text-gray-400">{{ formattedDatetime(transaction.createdAt) }}</span>
-                            <MoreInfoPopover title="What Is This?" description="This is the transaction date" placement="top"></MoreInfoPopover>
-                        </div>
+                        <Button
+                            size="sm"
+                            type="primary"
+                            class="w-full mb-4"
+                            @click="makePayment"
+                            v-if="isPendingPayment"
+                            :loading="isRenewTransactionPaymentLink">
+                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+                            </svg>
+                            <span>Make Payment</span>
+                        </Button>
 
-                        <div class="flex items-center space-x-2 text-xs">
-                            <span class="text-gray-400">{{ transaction.id }}</span>
-                            <MoreInfoPopover title="What Is This?" description="This is the transaction ID" placement="top"></MoreInfoPopover>
-                        </div>
+                        <Button
+                            size="sm"
+                            type="primary"
+                            class="w-full mb-4"
+                            @click="makePayment"
+                            v-if="isFailedPayment"
+                            :loading="isRenewTransactionPaymentLink">
+                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
+                            </svg>
+                            <span>Retry Payment</span>
+                        </Button>
+
+                        <template v-if="isPaid">
+
+                            <div class="flex items-center space-x-2 text-xs mb-2">
+                                <span class="text-gray-400">{{ formattedDatetime(transaction.createdAt) }}</span>
+                                <MoreInfoPopover title="What Is This?" description="This is the transaction date" placement="top"></MoreInfoPopover>
+                            </div>
+
+                            <div class="flex items-center space-x-2 text-xs">
+                                <span class="text-gray-400">{{ transaction.id }}</span>
+                                <MoreInfoPopover title="What Is This?" description="This is the transaction ID" placement="top"></MoreInfoPopover>
+                            </div>
+
+                        </template>
 
                     </div>
 
                 </div>
 
-                <div class="flex justify-center space-x-4">
+                <div class="flex justify-center">
 
-                    <Button @click="retryPayment" type="light" size="xs">
-                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
-                        </svg>
-                        <span>Retry Payment</span>
-                    </Button>
-
-                    <Button v-if="store" @click="navigateToStoreHome" type="primary" size="xs">
-                        <span>Go to {{ store.name }}</span>
+                    <Button v-if="store" @click="navigateToStoreHome" :type="isPaid ? 'primary' : 'light'" size="xs">
+                        <span>{{ store.name }}</span>
                         <svg class="w-4 h-4"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                             <path fill-rule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                         </svg>
@@ -128,14 +165,12 @@
         components: { Button, SpinningLoader, MoreInfoPopover },
         data() {
             return {
-                status: null,
-                storeHref: null,
                 transaction: null,
                 transactionHref: null,
-                isRenewTransactionPaymentLink: false,
                 isLoadingTransaction: false,
                 transactionDescription: null,
-                isGeneratingPaymentLink: false
+                isGeneratingPaymentLink: false,
+                isRenewTransactionPaymentLink: false,
             };
         },
         computed: {
@@ -193,11 +228,12 @@
                 });
 
             },
-            retryPayment() {
-                if(!this.dpoPaymentLinkHasExpired) {
+            makePayment() {
+                if (this.dpoPaymentLinkHasExpired) {
                     this.renewTransactionPaymentLink();
-                }else{
-                    window.location.href = this.dpoPaymentUrl;
+                } else {
+                    this.isRenewTransactionPaymentLink = true;
+                    window.location.assign(this.dpoPaymentUrl);
                 }
             },
             renewTransactionPaymentLink() {
@@ -221,7 +257,6 @@
 
                     }
 
-
                 }).catch(errorException => {
 
                     this.isRenewTransactionPaymentLink = false;
@@ -235,62 +270,10 @@
                     name: 'show-store-home',
                     params: { 'store_href': this.store._links.showStore }
                 });
-            },
-
-
-
-            payPricingPlan() {
-
-                if(this.isGeneratingPaymentLink) return;
-
-                this.isGeneratingPaymentLink = true;
-
-                let params = {
-                    'storeId': this.store.id,
-                    'paymentMethodType': 'dpo'
-                };
-
-                if(this.store) {
-                    params['storeId'] = this.store.id
-                }
-
-                postApi(this.pricingPlan._links['payPricingPlan'], params).then(response => {
-
-                    if(response.status == 200 && response.data['successful']) {
-
-                        const transaction = response.data.transaction;
-                        const dpoPaymentUrl = transaction.metadata.dpoPaymentUrl;
-
-                        window.location.href = dpoPaymentUrl;
-
-                    }else{
-
-                        this.isGeneratingPaymentLink = false;
-
-                    }
-
-
-                }).catch(errorException => {
-
-                    this.isGeneratingPaymentLink = false;
-                    this.setServerFormErrors(errorException);
-
-                });
-
             }
         },
         created() {
-
-            this.status = this.$route.query.status;
-            this.storeHref = this.$route.query.storeHref;
             this.transactionHref = this.$route.query.transactionHref;
-
-            if(this.status == 'successful') {
-                this.transactionDescription = 'Your payment was successful';
-            }else{
-                this.transactionDescription = 'Your payment failed';
-            }
-
             this.showTransaction();
         }
     };
